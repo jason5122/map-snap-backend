57af28f9786998fb5e533dda26c3d11917c4783b
Jason Han <jjhan@alum.vassar.edu>
2023-12-31 14:33:45 -0500

    Use same port for local testing and Docker

--- important-commands.sh
+++ important-commands.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker build --tag docker-rust-image .
-docker run --detach --publish 3001:8000 --name docker-rust-container docker-rust-image
+docker run --detach --publish 8000:8000 --name docker-rust-container docker-rust-image
 
 docker stop docker-rust-container
 docker start docker-rust-container
